8f797947bc0895f5c835c2cc0edfc503ffe1ff45
Bùi Thành Nhân <hi@imnhan.com>
2020-04-30 16:56:12 +0700

    load template & card list

--- src/cardman/model.py
+++ src/cardman/model.py
@@ -1,9 +1,21 @@
+from pathlib import Path
+
+CARDS_DIR = Path("cards")
+TEMPLATES_DIR = Path("templates")
+
+
 class Model:
     cards = []
+    templates = []
 
     def __init__(self):
-        self.cards = [
-            {"name": "foo.card", "content": "foo"},
-            {"name": "bar.card", "content": "bar"},
+        self.load_card_list()
+        self.load_template_list()
+
+    def load_card_list(self):
+        self.cards = [p for p in CARDS_DIR.iterdir() if str(p).endswith(".card")]
+
+    def load_template_list(self):
+        self.templates = [
+            p for p in TEMPLATES_DIR.iterdir() if str(p).endswith(".html")
         ]
-        self.templates = ["template1", "template2"]
--- src/cardman/view.py
+++ src/cardman/view.py
@@ -6,19 +6,24 @@ from .model import Model
 class View:
     model: Model
     pw: ttk.PanedWindow
+    cards_tree: ttk.Treeview
 
     def __init__(self, root: Tk, model: Model):
+        self.model = model
+
         Grid.rowconfigure(root, 0, weight=1)
         Grid.columnconfigure(root, 0, weight=1)
 
         # Top-level horizontal PanedWindow
         pw = ttk.PanedWindow(root, orient=HORIZONTAL)
+        self.pw = pw
         pw.grid(column=0, row=0, sticky="nesw")
 
         # Card selection
-        cards_tree = ttk.Treeview(pw, show="tree")
-        for card in model.cards:
-            cards_tree.insert("", 0, text=card["name"])
+        cards_tree = ttk.Treeview(pw, show="tree", selectmode="browse")
+        self.cards_tree = cards_tree
+        self.render_card_list()
+        cards_tree.bind("<<TreeviewSelect>>", self.on_select_card)
         pw.add(cards_tree)
 
         # Main text editor
@@ -31,16 +36,28 @@ class View:
 
         ## Template picker
         templates_tree = ttk.Treeview(vpw, show="tree")
-        for template in model.templates:
-            templates_tree.insert("", 0, text=template)
+        self.templates_tree = templates_tree
+        self.render_template_list()
+        cards_tree.bind("<<TreeviewSelect>>", self.on_select_template)
         vpw.add(templates_tree)
 
         ## Preview
         preview_frame = ttk.LabelFrame(vpw, text="Preview:", height=300, width=300)
         vpw.add(preview_frame)
 
-        # Housekeeping
         style = ttk.Style()
         style.theme_use("clam")
-        self.model = model
-        self.pw = pw
+
+    def render_card_list(self):
+        for card in self.model.cards:
+            self.cards_tree.insert("", "end", text=card.parts[-1], iid=card)
+
+    def render_template_list(self):
+        for template in self.model.templates:
+            self.templates_tree.insert("", "end", text=template.parts[-1], iid=template)
+
+    def on_select_card(self, ev):
+        print("selection is", ev.widget.selection())
+
+    def on_select_template(self, ev):
+        print("selection is", ev.widget.selection())
